0eeb9449bea3c6b3e2ce3f684876ac11b856dd9d
teoangelyn <teo.ningzhi.angelyn@dhs.sg>
2012-07-13 18:56:19 +0800

    third commit

--- carplate.py
+++ carplate.py
@@ -1,52 +1,71 @@
 # carplate.py
 
-weight = [14, 2, 12, 2, 11, 1]
-checksum_alphabet = ['A', 'Y', 'U', 'S', 'P', 'L', 'J', 'G', 'D', 'B', 'Z', 'X', 'T', 'R', 'M', 'K', 'H', 'E', 'C']
 
+entry = input("Input carplate number in uppercase: ")
+carplate = list(entry)
 
-carplate = list(input("Input carplate number: "))
+def check_digit_carplate(carplate):
+    for i in carplate:    
+    # remove first alphabet if carplate starts with 'S'
+        if carplate[0] == 'S':
+            del carplate[0]
 
-for i in carplate:
-# remove first alphabet
-    if carplate[0] == 'S':
-        carplate_n = carplate[1:-1]
-# insert 0 if there are only 3 numbers
-    if len(carplate_n) == 5:
-        carplate_n.insert(2,'0')
+    carplate = carplate[0:-1]
 
-checksum = 0
-alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'
-        'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-        'W', 'X', 'Y', 'Z']
-number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-           17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+    # insert 0 before first numerical digit if there are only 3 numbers
+    if len(carplate) == 5:
+        carplate.insert(2,'0')
+        
+    # remove last alphabet
 
-# convert alphabet prefixes to numbers
-for i in range(0, 2):
-    for e in range(0,25):
-        if carplate_n[i] == alphabet[e]:
-            carplate_n[i] = number_list[e]
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+            'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+            'W', 'X', 'Y', 'Z']
+    number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+               17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
 
-for i in range(0,7):
-    checksum = checksum + int(carplate_n[i]) * int(weight[i])
-# compute checksum using modulo
-    checksum = checksum % 19
+    # convert alphabet prefixes to numbers
+    for a in range(0, 2):
+        for e in range(0, 25):
+            if carplate[a] == alphabet[e]:
+                carplate[a] = number_list[e]
 
-def is_valid_carplate(number):
+ 
+    # initialise weights
+    weights = [14, 2, 12, 2, 11, 1]
+    # initialise sum of products
+    sum_of_products = 0
+
+    for i in range (0, len(weights)):
+        
+        sum_of_products = sum_of_products + int(carplate[i]) * weights[i]
+    # compute checksum using modulo
+    return sum_of_products % 19
+
+def is_valid_carplate(carplate):
+    checksum_alphabet = ['A', 'Y', 'U', 'S', 'P', 'L', 'J', 'G', 'D', 'B', 'Z', 'X', 'T', 'R', 'M', 'K', 'H', 'E', 'C']
     # validate carplate number
-        if checksum == carplate[-1]:
-            return True
-        else:
-            return False
-            print("Correct checksum alphabet:", checksum_alphabet[checksum])
+    if checksum_alphabet[check_digit_carplate(carplate)] == list(entry)[-1]:
+        return True
+    else:
+        return False
 
-print (is_valid_carplate(carplate))
+if is_valid_carplate(carplate) is False:
+    checksum_alphabet = ['A', 'Y', 'U', 'S', 'P', 'L', 'J', 'G', 'D', 'B', 'Z', 'X', 'T', 'R', 'M', 'K', 'H', 'E', 'C']
+
+    print("Invalid carplate number. Correct checksum alphabet: ", checksum_alphabet[check_digit_carplate(carplate)])
+    try: 
+        outfile = open("INVALID.DAT", "w")
+        outfile.write(carplate + "\n")
+    except IOError:
+        print("Cannot write to file 'INVALID.DAT'")
+else:
+    print("Valid carplate number")
 
 
-##if is_valid_carplate(carplate) is False:
-##    try: 
-##        outfile = open("INVALID.DAT", "w")
-##        outfile.write(carplate + "\n")
-##    except Error:
-##        print("Cannot write to file 'INVALID.DAT'")
-##
+
+is_valid_carplate(carplate)
+
+
+
+
